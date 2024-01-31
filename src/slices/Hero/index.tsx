4eb0879df7a2f854040546b260962725e239d8ba
Jay Korat <koratjay03@gmail.com>
2024-01-31 16:27:06 +0530

    staring setup

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -15,18 +15,9 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-     {
-      slice.primary.first_name
-      
-     }
-     {
-      slice.primary.last_name
-      
-     }
-     {
-      slice.primary.tag_line
-      
-     }
+     <span className="text-red-500">{slice.primary.first_name}</span>
+     <>{slice.primary.last_name}</>
+     <>{slice.primary.tag_line}</>
     </section>
   );
 };
