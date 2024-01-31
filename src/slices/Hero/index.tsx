d83dfcc03c60bf5a26f364e3fb2eae608a008d4c
Jay Korat <koratjay03@gmail.com>
2024-01-31 16:57:30 +0530

    styling of name

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -15,9 +15,20 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-     <span className="text-red-500">{slice.primary.first_name}</span>
-     <>{slice.primary.last_name}</>
-     <>{slice.primary.tag_line}</>
+
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center" >
+
+        <div className="col-start-1 md:row-start-1">
+          <h1 className="mb-8 text-[clamp(1rem,20vmin,20rem)] font-extrabold leading-none " 
+              aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
+            <span className="block text-slate-300">{slice.primary.first_name}</span>
+            <span className="-mt-[.2rem] block text-slate-500">{slice.primary.last_name}</span>
+            
+          </h1>
+          <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl 
+                           font-bold uppercase tracking-[.2rem] text-transparent opacity-100 md:text-4xl ">{slice.primary.tag_line}</span >
+        </div>
+      </div>
     </section>
   );
 };
