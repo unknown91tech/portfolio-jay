da740b5aed01c9c0f6268d16185a50ddcb03cb30
unknown91tech <koratjay22@gmail.com>
2024-02-03 15:22:46 +0530

    till audio done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-black text-slate-100">
+    <html lang="en" className="bg-neutral-900 text-slate-100">
       <body className={urbanist.className}>{children}</body>
     </html>
   );
